60c899bd2ec97ad84a352138e4031b130e09ce82
Filipe Santos <filipesantos1999@hotmail.com>
2025-10-04 18:14:48 +0100

    docs: Add real hex string in examples

--- examples/decode_tcp.zig
+++ examples/decode_tcp.zig
@@ -16,10 +16,10 @@ pub fn main() !void {
     const zeltonika = try Zeltonika.init(allocator, config);
     defer zeltonika.deinit();
 
-    const encoded_hex_tcp = "";
+    const encoded_hex_tcp = "000000000000003608010000016B40D8EA30010000000000000000000000000000000105021503010101425E0F01F10000601A014E0000000000000000010000C7CF";
     const encoded_tcp_len = encoded_hex_tcp.len / 2;
 
-    const encoded_tcp: [encoded_tcp_len]u8 = undefined;
+    var encoded_tcp: [encoded_tcp_len]u8 = undefined;
 
     _ = try std.fmt.hexToBytes(&encoded_tcp, encoded_hex_tcp);
 
