408f00b6032916f6cb386a524e8d64dcf3d3e69d
Filipe Santos <filipesantos1999@hotmail.com>
2026-01-25 21:51:29 +0000

    fix: Fix identation

--- utils/bytebuffer/bytebuffer.v
+++ utils/bytebuffer/bytebuffer.v
@@ -150,8 +150,8 @@ pub fn (mut bb ByteBuffer) put[T](value T) ! {
 	  bytes = value.bytes()
 	} $else $if T is $array_dynamic {
     for element in value {
-  		bytes << binary.encode_binary(element, bb.encoding_config)!
-  	}
+      bytes << binary.encode_binary(element, bb.encoding_config)!
+    }
 	} $else {
 	  bytes = binary.encode_binary(value, bb.encoding_config)!
 	}
