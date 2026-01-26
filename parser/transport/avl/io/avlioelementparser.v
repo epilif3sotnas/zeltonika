1ac9d205056c50035b59997f7a196275b5493c1e
Filipe Santos <filipesantos1999@hotmail.com>
2026-01-26 07:13:47 +0000

    fix: Fix identation

--- parser/transport/avl/io/avlioelementparser.v
+++ parser/transport/avl/io/avlioelementparser.v
@@ -106,7 +106,7 @@ fn (_ &AvlIoElementParser) encode_n_io_elements[N, K](
 	for k, v in avl_n_element {
 	  buffer.put(K(k))!
 
-		match avl_io_element {
+  	match avl_io_element {
    	  .element1 { buffer.put(utils_arrays.array_to_fixed_1[u8](v))! }
    	  .element2 { buffer.put(utils_arrays.array_to_fixed_2[u8](v))! }
    	  .element4 { buffer.put(utils_arrays.array_to_fixed_4[u8](v))! }
